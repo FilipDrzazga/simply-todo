b698c31cfccc881839bc96ffb06f45a3a2579c13
Filip <43039909+Urfisior@users.noreply.github.com>
2023-02-12 21:40:41 +0000

    swap all context providers and global style to index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
 import WelcomeScreen from "./Components/WelcomeScreen";
 
-import { GlobalStyle } from "./styled/globalStyle";
-import { ThemeProvider } from "styled-components";
-import { rules } from './styled/theme';
-
 const App = () => {
   return (
-    <ThemeProvider theme={rules}>
-      <GlobalStyle/>
-      <WelcomeScreen/>
-    </ThemeProvider>
+    <WelcomeScreen/>
   )
 };
 
