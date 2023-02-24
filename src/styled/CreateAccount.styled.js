8a8ebad802456c7488b28ca39b53cde28e512013
Filip <43039909+Urfisior@users.noreply.github.com>
2023-02-24 11:34:12 +0000

    add fontAwesome and createUser account via email&password in CreateAccount comp.

--- src/styled/CreateAccount.styled.js
+++ src/styled/CreateAccount.styled.js
@@ -4,29 +4,29 @@ const Section = styled.section`
     position:relative;
     width:100%;
     height:100%;
-    header{
-        position: absolute;
-        top:0;
-        left:0;
-        transform:translateY(60%);
-        width:100%;
-        display:flex;
-        align-items:center;
-        flex-direction:column;
-        flex-wrap:wrap;
-        h1{
-            font-size:${({theme})=>theme.fontSizes[4]};
-            color:${({theme})=>theme.colors.darkPurple};
-        }
-        h2{
-            font-size:${({ theme }) => theme.fontSizes[1]};
-            font-weight: 400;
-            color:${({theme})=>theme.colors.darkPurple};
-        }
-    }
     img{
         width:100%;
         height:100vh;
+    }
+`
+const Header = styled.header`
+    position: absolute;
+    top:0;
+    left:0;
+    transform:translateY(60%);
+    width:100%;
+    display:flex;
+    align-items:center;
+    flex-direction:column;
+    flex-wrap:wrap;
+    h1{
+        font-size:${({theme})=>theme.fontSizes[4]};
+        color:${({theme})=>theme.colors.darkPurple};
+    }
+    h2{
+        font-size:${({ theme }) => theme.fontSizes[1]};
+        font-weight: 400;
+        color:${({theme})=>theme.colors.darkPurple};
     }
 `
 
@@ -81,4 +81,4 @@ const ButtonContainer = styled.div`
     }
 `
 
-export { Section, Form, InputsContainer, ButtonContainer };
+export { Section, Header, Form, InputsContainer, ButtonContainer };
